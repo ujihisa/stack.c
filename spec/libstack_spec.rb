b9f81c05fb2fb10df4babd8a4b471de3e8422a18
Kenta Murata <mrkn@mrkn.jp>
2011-09-02 14:20:45 +0900

    add the code example of 'the initial state has zero index'.

--- spec/libstack_spec.rb
+++ spec/libstack_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe LibStack do
+  describe 'the initial state' do
+    subject do
+      described_class.newstack(42)
+    end
+
+    it "has zero index" do
+      subject.i.should eq 0
+    end
+  end
+
   describe '.newstack' do
     context 'with 42' do
       it 'create a new Stack of size 42' do
